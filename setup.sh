1d19b48112cffebef2269574e00d9e5c63237b89
h8d13 <hadean-eon-dev@proton.me>
2026-07-16 18:45:07 +0200

    misc
    Signed-off-by: h8d13 <hadean-eon-dev@proton.me>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-# more simplistic setup
-# usually ive already set root pw at this step
-pacman -S --needed --noconfirm zram-generator ufw micro
+# more simplistic setup made for my laptop
+# usually ive already set root pw at this step `passwd root`
+
+pacman -S --needed --noconfirm zram-generator ufw micro terminus-font
 
 # art above, stock \S{PRETTY_NAME} \r (\l) line stays at the bottom.
 # ':::' guard keeps reruns from stacking copies; quoted heredoc
@@ -44,14 +45,47 @@ fi
 
 echo "[zram0]" > /etc/systemd/zram-generator.conf
 echo "EDITOR=micro" >> /etc/environment
+echo "KEYMAP=be-latin1" >> /etc/vconsole.conf
+echo "FONT=ter-p24n" >> /etc/vconsole.conf
+
 ufw enable
 systemctl enable ufw
 
-pacman -S --needed --noconfirm seatd sway foot
+# system done; user next, no sudo priv
+
+pacman -S --needed --noconfirm \
+	seatd \
+	sway \
+	foot \
+	# essentials done
+	pipewire-pulse \
+	wireplumber \
+	sof-firmware \
+	pavucontrol \
+	pipewire-alsa \
+	pipewire-jack \
+	gst-plugin-pipewire \
+	libpulse \
+	# sound done
+       	brightnessctl \
+	grim \
+	slurp \
+	xorg-xwayland
+
 systemctl enable --now seatd
+systemctl --global enable pipewire.socket pipewire-pulse.socket wireplumber.service
+
 useradd -m hadean && usermod -aG seat hadean
-echo "sway" > /home/hadean/.bash_profile
+
+echo "export XKB_DEFAULT_LAYOUT=be" >> /home/hadean/.bash_profile
+echo "export XKB_DEFAULT_VARIANT=nodeadkeys" >> /home/hadean/.bash_profile
+echo "exec sway" >> /home/hadean/.bash_profile
+
 # -m creates the home, -aG is standard for groups idek
 # all got left to do is passwd hadean from a root shell
 # plus probably some sound shenanigans
 # this has no sudo or anything the profile is just sway
+
+chown -R hadean:hadean /home/hadean
+
+# thats it folks not clone dotfiles; done?
